f7f0282942e0a2614184d1a2c5ad04eb2ec98420
Antony J <antony.jesudhason@verivox.com>
2020-06-15 17:33:01 +0200

    Updates

---
+++ precache-manifest.cbb24b8da53a3dbd85e5892507131fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b28e1722d000c650fa2577002f35de9",
+    "url": "/covid-19-simple-dashboard/index.html"
+  },
+  {
+    "revision": "2b6871065c932f183860",
+    "url": "/covid-19-simple-dashboard/static/css/main.d4364c9b.chunk.css"
+  },
+  {
+    "revision": "ed4f7113c02b78fe8e50",
+    "url": "/covid-19-simple-dashboard/static/js/2.dc8ac332.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-simple-dashboard/static/js/2.dc8ac332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6871065c932f183860",
+    "url": "/covid-19-simple-dashboard/static/js/main.453e6999.chunk.js"
+  },
+  {
+    "revision": "1420141783d5c8547806",
+    "url": "/covid-19-simple-dashboard/static/js/runtime-main.d7568df0.js"
+  }
+]);
